56fd87c9f9491c979122ff56de2ad11ef887e818
Yusuf Kortobi <kort8325@bears.unco.edu>
2020-10-16 16:09:18 -0600

    Allowed Hosts

--- project5/config/settings.py
+++ project5/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h7fr=ihjkzam2%hp&90=wm1bamt%7fi%_mn&+d*_(v$fkz)_x!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
